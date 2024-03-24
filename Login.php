40fddfeeca7e4c5f5edc081cb4e23f4c6bd1bfd6
Hoson Hasqoor <85869139+Hoson02y@users.noreply.github.com>
2024-03-24 20:09:49 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,39 +1,51 @@
-
-
 <?php
+session_start(); 
 
 include_once 'DBconn.php'; 
 
 if (isset($_POST['submit'])) {
-    if (empty($_POST['email']) || empty($_POST['password'])) {
-        echo "<script>alert('One or more inputs are empty');</script>";
-    } else {
+    if (!empty($_POST['email']) && !empty($_POST['password'])) {
         $email = $_POST['email'];
-        $password = $_POST['password']; 
+        $password = $_POST['password'];
 
-        $login = $conn->query("SELECT * FROM customers WHERE email = '$email'");
-        $login->execute();
+        $stmtCustomer = $conn->prepare("SELECT * FROM customers WHERE email = :email");
+        $stmtSupplier = $conn->prepare("SELECT * FROM suppliers WHERE email = :email");
 
-        $fetch = $login->fetch(PDO::FETCH_ASSOC);
+        $stmtCustomer->bindParam(':email', $email);
+        $stmtSupplier->bindParam(':email', $email);
 
-        if($login->rowCount() > 0) {
+        $stmtCustomer->execute();
+        $stmtSupplier->execute();
 
-            if(password_verify($password,$fetch['password'])){
-              $_session['username'] =$fetch['username'];
-              $_session['user_id'] =$fetch['id'];
+        $userType = null;
+        $user = null;
 
-              header("location:index.php");
-            }else {
-                echo "<script>atert('email or password are wrong');</script>";
+        if ($stmtCustomer->rowCount() > 0) {
+            $userType = 'customer';
+            $user = $stmtCustomer->fetch(PDO::FETCH_ASSOC);
+        } elseif ($stmtSupplier->rowCount() > 0) {
+            $userType = 'supplier';
+            $user = $stmtSupplier->fetch(PDO::FETCH_ASSOC);
+        }
+
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['user_type'] = $userType; 
+
+            
+            if ($userType == 'customer') {
+                header("Location: index.php"); 
+            } else if ($userType == 'supplier') {
+                header("Location: header.php"); 
             }
-            } else {
-                echo "<script>atert('email or password are wrong');</script>";
-
-            } 
-        
-
+            exit();
+        } else {
+            echo "<script>alert('Email or password is incorrect.');</script>";
+        }
+    } else {
+        echo "<script>alert('One or both fields are empty.');</script>";
     }
-
 }
 ?>
 
@@ -41,18 +53,18 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <title>Log In</title>
 </head>
 <body>
 
-<form action="" method="post"> <!-- The action should point to the PHP script, if separate -->
+<form action="" method="post">
     <label for="email">Email:</label>
     <input type="email" id="email" name="email" required><br><br>
 
     <label for="password">Password:</label>
     <input type="password" id="password" name="password" required><br><br>
 
-    <input type="submit" name="submit" value="Login">
+    <input type="submit" name="submit" value="Submit">
 </form>
 
 </body>
